88221769586f2eebef5460278a7316b4798960bd
BruceICzw <h220396z@hit.ac.zw>
2023-10-19 23:38:10 +0200

    Create Project structure

---
+++ e-commerce/src/components/Buttons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Buttons() {
+    return (
+        <div>
+            Click me
+        </div>
+    )
+}
+
+export default Buttons;
---
+++ e-commerce/src/db/data.js
@@ -0,0 +1,29 @@
+import { AiFillStar } from "react-icons/ai";
+
+const data = [
+    {
+        img: "https://m.media-amazon.com/images/l/6125yAfsJKL._AC_UX575_.jpg",
+        title: "Nike Air Monarch IV",
+        star: "<AiFillStar className='rating-star' />",
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Nike",
+        color: "white",
+        category: "sneakers",
+    },
+    {
+        img: "https://m.media-amazon.com/images/l/6125yAfsJKL._AC_UX575_.jpg",
+        title: "Nike Air Monarch IV",
+        star: "<AiFillStar className='rating-star' />",
+        reviews: "(123 reviews)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company: "Nike",
+        color: "white",
+        category: "sneakers",
+    }
+    // add more objects here if needed
+];
+
+export default data;
