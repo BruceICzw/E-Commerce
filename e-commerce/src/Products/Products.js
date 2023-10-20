d6829bdbe83964af081cba27f3fc31b8f3da5caf
BruceICzw <h220396z@hit.ac.zw>
2023-10-20 14:36:49 +0200

    Add Products

--- e-commerce/src/Products/Products.js
+++ e-commerce/src/Products/Products.js
@@ -1,9 +1,211 @@
 import React from 'react'
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagHeartFill } from "react-icons/bs";
 import './Products.css';
- function Products() {
-  return (
-    <div>Products</div>
-  )
+function Products() {
+  return <>
+    <section className="card-container">
+      <section className="card">
+        <img src="http://via.placeholder.com/640x360" alt="Picture" className='card-img' />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>200
+
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className='bag-icon' />
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="card">
+        <img src="http://via.placeholder.com/640x360" alt="Picture" className='card-img' />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>200
+
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className='bag-icon' />
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="card">
+        <img src="http://via.placeholder.com/640x360" alt="Picture" className='card-img' />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>200
+
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className='bag-icon' />
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="card">
+        <img src="http://via.placeholder.com/640x360" alt="Picture" className='card-img' />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>200
+
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className='bag-icon' />
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="card">
+        <img src="http://via.placeholder.com/640x360" alt="Picture" className='card-img' />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>200
+
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className='bag-icon' />
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="card">
+        <img src="http://via.placeholder.com/640x360" alt="Picture" className='card-img' />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>200
+
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className='bag-icon' />
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="card">
+        <img src="http://via.placeholder.com/640x360" alt="Picture" className='card-img' />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>200
+
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className='bag-icon' />
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="card">
+        <img src="http://via.placeholder.com/640x360" alt="Picture" className='card-img' />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>200
+
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className='bag-icon' />
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className="card">
+        <img src="http://via.placeholder.com/640x360" alt="Picture" className='card-img' />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <AiFillStar className='rating-star' />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$300</del>200
+
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className='bag-icon' />
+            </div>
+          </section>
+        </div>
+      </section>
+    </section>
+  </>
+
 }
 
 export default Products;
