d8b491846799e038206cd8e57643feeae123b749
BruceICzw <h220396z@hit.ac.zw>
2023-10-21 16:30:02 +0200

    Add Functionality

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,11 +1,80 @@
+import { useState } from "react";
 import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 
+//DAATA STORE
+import products from './db/data'
+import Card from "./components/Card";
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null)
+
+  //input filter
+  const [query, setQuery] = useState("")
+
+  const handleInputChange = event => {
+    setQuery(event.target.value)
+  }
+
+
+  const filteredItems = products.filter((product) =>
+    product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+  );
+
+  // ---------------Radio Filters-----------------
+  const handleChange = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+
+  // ----------------Buttons Filter-----------------
+  const handleClick = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+
+
+  // ------------MAIN Function--------------
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    //filtering input items
+    if (query) {
+      filteredData = filteredItems;
+    }
+
+    //Seleccted Filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, newPrice, prevPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          newPrice={newPrice}
+          prevPrice={prevPrice}
+
+        />
+
+
+      ))
+  }
+  const result = filteredData(products, selectedCategory, query);
   return <>
-    <Sidebar />
+    <Sidebar handleChange={handleChange} />
     <Navigation />
     <Recommended />
     <Products />
