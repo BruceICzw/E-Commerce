8ba01b612d8a0ebe8dce8535e37353ba95ae41f8
BruceICzw <h220396z@hit.ac.zw>
2023-10-21 17:48:04 +0200

    Refactor Sidebar Items

--- e-commerce/src/Sidebar/Category/Category.js
+++ e-commerce/src/Sidebar/Category/Category.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import './Category.css'
 import Input from '../../components/Input';
-function Category() {
+function Category({ handleChange }) {
   return <div>
     <h2 className="sidebar-title">Category</h2>
     <div >
 
-      <Input />
+      <label className="sidebar-label-container">
+        <input onChange={handleChange} type="radio" name="test" value="" />
+        <span className="checkmark"></span>All
+      </label>
+
+      <Input
+        handleChange={handleChange}
+        value='sneakers'
+        title='Sneakers'
+        name='test'
+      />
+      <Input
+        handleChange={handleChange}
+        value='flats'
+        title='Flats'
+        name='test'
+      />
+      <Input
+        handleChange={handleChange}
+        value='sandles'
+        title='Sandals'
+        name='test'
+      />
+      <Input
+        handleChange={handleChange}
+        value='heels'
+        title='Heels'
+        name='test'
+      />
     </div>
   </div>
 
